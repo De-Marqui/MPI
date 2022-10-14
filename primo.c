0f82dbcc2506c5258e283ed64259a6f4a675cf05
Victor De Marqui <victor.demarqui@gmail.com>
2022-10-14 04:42:53 -0300

    Update primo.c

--- primo.c
+++ primo.c
@@ -61,11 +61,11 @@ void primeCalc(int lowerRange, int upperRange) {
 
 int main() {
    int number1, number2;
+   printf("\nDigite um intervalo de 2 numeros formato: [numero] [espaço] [numero]:\n");
    scanf("%d %d", &number1, &number2);
    MPI_Init(NULL , NULL);
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
    MPI_Comm_size(MPI_COMM_WORLD, &process_count);
-   printf("\nDigite um intervalo de 2 numeros formato: [numero] [espaço] [numero]:\n");
    primeCalc(number1, number2);
    MPI_Finalize();
 }
